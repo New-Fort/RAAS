c36a43332b513e3ceb7b1f3399f18be7f7da8e03
marceel <105522740+New-Fort@users.noreply.github.com>
2024-10-14 14:19:12 +0200

    Update Text Data

--- text_data.py
+++ text_data.py
@@ -1,1 +1,1 @@
-data = ["Ify i fory.", "Koniec głupawki.", "Kita. Last call.", "Panowie, przedszkolak by to zrobił.", "KAJAKI.", "To nic. Od wody się nie umiera.", "Człowiek małej wiary 😊"]
+data = ["Ify i fory.", "Koniec głupawki.", "Kita. Last call.", "Panowie, przedszkolak by to zrobił.", "KAJAKI.", "To nic. Od wody się nie umiera.", "Człowiek małej wiary 😊","Ale ty nie wiesz jeszcze czy ty jesteś homo czy hetero."]
