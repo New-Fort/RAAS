3f4a6e025f96fe8f35f011268ccc9ffa293208e1
Marcel Marchliński <kosmitamarcel@gmail.com>
2024-10-14 22:11:36 +0200

    ADDED KEYING TO THE REQUESTS Now, you can add a ?key=string, to REQUEST only the quotes that have the key in them.

--- main.py
+++ main.py
@@ -10,10 +10,23 @@ app.config['JSON_AS_ASCII'] = False
 
 @app.route("/get-quote")
 def get_quote():
-    randdata = random.randint(0, len(data)-1)
+    key = request.args.get("key")
+    curateddata = []
+
+    if key:
+        for quote in data:
+            if key in quote:
+                curateddata.append(quote)
+
+    if curateddata:
+        selected_quote = random.choice(curateddata)
+    else:
+        selected_quote = random.choice(data)  # Fallback to a random quote from the full data
+
     response_data = {
-        "quote": data[randdata]
+        "quote": selected_quote
     }
+
     response_json = json.dumps(response_data, ensure_ascii=False)
     return Response(response_json, content_type='application/json; charset=utf-8')
 
